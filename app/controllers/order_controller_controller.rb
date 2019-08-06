3e13d28c61de57ec229180523ea28d233a5ac86f
AJ De La Costa-CSUMB <ade-la-costa@csumb.edu>
2019-08-06 03:53:26 +0000

    Updated the Gemfile. Changed the db migration file to exclude the id as that was causing a rails error. Also updated the controller.

--- app/controllers/order_controller_controller.rb
+++ app/controllers/order_controller_controller.rb
@@ -1,2 +1,53 @@
 class OrderControllerController < ApplicationController
+    
+    # Create POST /orders
+    # Request body contains JSON data with the itemId
+    # and email of customer
+    def create
+        # Create the order 
+        @order = Order.new
+        @order.itemId = params[:itemId]
+        
+        # Invoke the customer service to retrieve the customer id using the customers
+        # email address
+        # Something like:
+        # @order.customerId = getCustomerId(params[:email])
+        
+        # All other data is optional
+        
+        # Description
+        if params.has_key?(:description)
+            # Assign it to the order
+            @order.description = params[:description]
+        else
+            @order.description = ""
+        end
+        
+        # Award
+        if params.has_key?(:award)
+            # Assign it to the order
+            @order.award = params[:award]
+        else
+            @order.award = 0.00
+        end
+        
+        # Price
+        if params.has_key?(:price)
+            # Assign it to the order
+            @order.price = params[:price]
+        else
+            @order.price = 0.00
+        end
+        
+        # Total
+        if params[:award] == 0 || @orders.award == 0
+            # Assign it to the order
+            @order.total = @order.price
+        elsif params.has_key?(:total)
+            # Assign it to the order
+            @order.total = params[:total]
+        else
+            @order.total = 0.00
+        end
+    end
 end
