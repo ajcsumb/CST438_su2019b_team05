d508f650830fc9566dc9f092e8a6b99fe0d1eaa0
AJ De La Costa-CSUMB <ade-la-costa@csumb.edu>
2019-08-06 03:59:50 +0000

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get '/orders/:id' => 'orders#get'
+  get '/orders/:id' => 'orders#getById'
   post '/orders' => 'orders#create'
 end
