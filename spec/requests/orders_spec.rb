50a2cc8d29160a84930d1c2ef35cfa6136ff6628
EC2 Default User <ec2-user@ip-172-31-37-123.us-east-2.compute.internal>
2019-08-14 03:41:09 +0000

    Updated Rspec

---
+++ spec/requests/orders_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+RSpec.describe "OrdersController", type: :request do
+  
+    describe "POST /orders" do
+        
+        it "Customer makes purchase" do
+            
+        #1. This is input data for orders_controller
+          order = { itemId: 1,                    
+                    email: 'dw@csumb.edu' } 
+                    
+          headers = {"CONTENT_TYPE" => "application/json" ,
+                     "ACCEPT" => "application/json"}    
+        
+        #2. Rspec double for Customer.getCustomerByEmail             
+          expect(Customer_Service).to receive(:getCustomerByEmail).with('dw@csumb.edu') do    
+            [ 200, {:id => 1, :award => 0 } ]
+          end 
+        
+        #3. Rspec double for Item.getItemById  
+          expect(Item_Service).to receive(:getItemById).with(1) do          
+            [ 200, { :id =>1, :description =>'jewelry item',
+                          :price => 175.00, :stockQty => 2 } ]
+          end 
+        
+        #4. Rspec double for Customer.putOrder  
+          allow(Customer_Service).to receive(:postOrder) do |order|      
+            expect(order.customerId).to eq 1 
+            201
+          end 
+          
+        #5. Rspec double for Item.putOrder
+          allow(Item_Service).to receive(:postOrder) do |order|         
+            expect(order.itemId).to eq 1
+            201
+          end 
+                    
+          post '/orders', params: order.to_json, headers: headers
+          
+          expect(response).to have_http_status(201)
+          order_json = JSON.parse(response.body)
+          expect(order_json).to include('itemId'=> 1, 
+                                        'description'=>'jewelry item',
+                                        'customerId'=> 1 ,
+                                        'price'=> 175.00,
+                                        'award'=> 0,
+                                        'total'=> 175.00 )
+           
+            end
+            
+        it "Customer fails purchase because item stockQty is 0" do
+            
+        #1. This is input data for orders_controller
+          order = { itemId: 1,                    
+                    email: 'dw@csumb.edu' } 
+                    
+          headers = {"CONTENT_TYPE" => "application/json" ,
+                     "ACCEPT" => "application/json"}    
+        
+        #2. Rspec double for Customer.getCustomerByEmail             
+          expect(Customer_Service).to receive(:getCustomerByEmail).with('dw@csumb.edu') do    
+            [ 200, {:id => 1, :award => 0 } ]
+          end 
+        
+        #3. Rspec double for Item.getItemById  
+           expect(Item_Service).to receive(:getItemById).with(1) do          
+            [ 200, { :id => 1, :description => 'jewelry item',
+                          :price => 175.00, :stockQty => 0.0 } ]
+          end 
+                    
+          post '/orders', params: order.to_json, headers: headers
+          
+          expect(response).to have_http_status(200)
+          order_json = JSON.parse(response.body)
+          expect(order_json).to include( {"error" => "Item is out of stock", "status" => 400} )
+            end
+        end
+    end
